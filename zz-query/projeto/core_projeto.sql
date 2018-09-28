fe520f43bda98c51833795334bbdaccd1b333383
oscip-iabs <iabs.sistema@gmail.com>
2018-09-28 11:53:53 -0300

    sql

---
+++ zz-query/projeto/core_projeto.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.projeto_abrangencia_atuacao (id, date_created, texto, descricao) VALUES (1, '2018-06-05 13:45:39.720000', 'Meio Ambiente Natural', 'Atua no Meio Ambiente Natural');
+INSERT INTO public.projeto_abrangencia_atuacao (id, date_created, texto, descricao) VALUES (2, '2018-06-05 13:44:41.080000', 'Meio Ambiente Histórico Cultural', 'Atua no Meio Ambiente Histórico Cultural');
+INSERT INTO public.projeto_abrangencia_atuacao (id, date_created, texto, descricao) VALUES (3, '2018-06-05 13:45:39.720000', 'Meio Ambiente Urbanístico', 'Atua no Meio Ambiente Urbanístico');
+
+INSERT INTO public.projeto_nucleo (id, date_created, texto, responsavel_id) VALUES (1, '2018-05-15 01:28:43.195000', 'Nucleo Xingo', 1);
+INSERT INTO public.projeto_nucleo (id, date_created, texto, responsavel_id) VALUES (2, '2018-05-15 01:28:43.195000', 'Nucleo Sustentabilidade', 1);
+INSERT INTO public.projeto_nucleo (id, date_created, texto, responsavel_id) VALUES (3, '2018-05-15 01:28:43.195000', 'Nucleo Agroflorestal', 1);
