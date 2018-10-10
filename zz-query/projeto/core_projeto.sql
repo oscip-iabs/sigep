6d2bb489674bdb1308ad831119ef9ad9a0229e6c
oscip-iabs <iabs.sistema@gmail.com>
2018-10-10 17:23:09 -0300

    urls

--- zz-query/projeto/core_projeto.sql
+++ zz-query/projeto/core_projeto.sql
@@ -1,7 +1,24 @@
-INSERT INTO public.projeto_abrangencia_atuacao (id, date_created, texto, descricao) VALUES (1, '2018-06-05 13:45:39.720000', 'Meio Ambiente Natural', 'Atua no Meio Ambiente Natural');
-INSERT INTO public.projeto_abrangencia_atuacao (id, date_created, texto, descricao) VALUES (2, '2018-06-05 13:44:41.080000', 'Meio Ambiente Histórico Cultural', 'Atua no Meio Ambiente Histórico Cultural');
-INSERT INTO public.projeto_abrangencia_atuacao (id, date_created, texto, descricao) VALUES (3, '2018-06-05 13:45:39.720000', 'Meio Ambiente Urbanístico', 'Atua no Meio Ambiente Urbanístico');
+INSERT INTO public.projeto_nucleo (id, date_created, texto, responsavel_id) VALUES (1, '2018-10-02 16:12:32.657000', 'Diálogos Sociais e Gestao de Conflitos', 1);
+INSERT INTO public.projeto_nucleo (id, date_created, texto, responsavel_id) VALUES (2, '2018-10-02 16:12:32.657000', 'Energia Inclusiva', 1);
+INSERT INTO public.projeto_nucleo (id, date_created, texto, responsavel_id) VALUES (3, '2018-10-02 16:12:32.657000', 'Desenvolvimento Rural e Tecnologias Sociais', 1);
+INSERT INTO public.projeto_nucleo (id, date_created, texto, responsavel_id) VALUES (4, '2018-10-02 16:12:32.657000', 'Turismo Sustentável', 1);
+INSERT INTO public.projeto_nucleo (id, date_created, texto, responsavel_id) VALUES (5, '2018-10-02 16:12:32.657000', 'Meio Ambiente e Áreas Protegidas', 1);
+INSERT INTO public.projeto_nucleo (id, date_created, texto, responsavel_id) VALUES (6, '2018-10-02 16:12:32.657000', 'Aquicultura e Pesca Sustentáveis', 1);
 
-INSERT INTO public.projeto_nucleo (id, date_created, texto, responsavel_id) VALUES (1, '2018-05-15 01:28:43.195000', 'Nucleo Xingo', 1);
-INSERT INTO public.projeto_nucleo (id, date_created, texto, responsavel_id) VALUES (2, '2018-05-15 01:28:43.195000', 'Nucleo Sustentabilidade', 1);
-INSERT INTO public.projeto_nucleo (id, date_created, texto, responsavel_id) VALUES (3, '2018-05-15 01:28:43.195000', 'Nucleo Agroflorestal', 1);
+INSERT INTO public.projeto_tipo_instrumento (id, date_created, nome) VALUES (1, '2018-10-03 09:37:35.842000', 'Não necessita');
+INSERT INTO public.projeto_tipo_instrumento (id, date_created, nome) VALUES (2, '2018-10-03 09:38:01.690000', 'Contrato');
+INSERT INTO public.projeto_tipo_instrumento (id, date_created, nome) VALUES (3, '2018-10-03 09:38:01.690000', 'Acordo');
+INSERT INTO public.projeto_tipo_instrumento (id, date_created, nome) VALUES (4, '2018-10-03 09:38:01.690000', 'Outro');
+
+INSERT INTO public.projeto_tipo_contratacao (id, date_created, texto) VALUES (1, '2018-10-02 16:33:10.470000', 'Edital');
+INSERT INTO public.projeto_tipo_contratacao (id, date_created, texto) VALUES (2, '2018-10-02 16:33:10.470000', 'Convenio');
+INSERT INTO public.projeto_tipo_contratacao (id, date_created, texto) VALUES (3, '2018-10-02 16:33:10.470000', 'Parceria');
+INSERT INTO public.projeto_tipo_contratacao (id, date_created, texto) VALUES (4, '2018-10-02 16:33:10.470000', 'Contrato');
+INSERT INTO public.projeto_tipo_contratacao (id, date_created, texto) VALUES (5, '2018-10-02 16:33:10.470000', 'Outros');
+
+INSERT INTO public.projeto_nucleo (date_created, texto, responsavel_id) VALUES ('2018-10-02 16:12:32.657000', 'Diálogos Sociais e Gestao de Conflitos', 1);
+INSERT INTO public.projeto_nucleo (date_created, texto, responsavel_id) VALUES ('2018-10-02 16:12:32.657000', 'Energia Inclusiva', 1);
+INSERT INTO public.projeto_nucleo (date_created, texto, responsavel_id) VALUES ('2018-10-02 16:12:32.657000', 'Desenvolvimento Rural e Tecnologias Sociais', 1);
+INSERT INTO public.projeto_nucleo (date_created, texto, responsavel_id) VALUES ('2018-10-02 16:12:32.657000', 'Turismo Sustentável', 1);
+INSERT INTO public.projeto_nucleo (date_created, texto, responsavel_id) VALUES ('2018-10-02 16:12:32.657000', 'Meio Ambiente e Áreas Protegidas', 1);
+INSERT INTO public.projeto_nucleo (date_created, texto, responsavel_id) VALUES ('2018-10-02 16:12:32.657000', 'Aquicultura e Pesca Sustentáveis', 1);
